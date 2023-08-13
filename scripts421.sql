48e69cfc2b7f5be6277a13a24e825695c0f1c910
BuenosDiasGente <morkovka03121981@gmail.com>
2023-08-13 17:16:51 +0300

    SQL

---
+++ scripts421.sql
@@ -0,0 +1,5 @@
+ALTER TABLE student ADD CONSTRAINT age_constraint CHECK ( age>16 );
+ALTER TABLE student ADD CONSTRAINT name_constraint UNIQUE(name);
+ALTER TABLE student ALTER COLUMN name SET NOT NULL;
+ALTER TABLE faculty ADD CONSTRAINT name_color_unique UNIQUE (name,color);
+ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
