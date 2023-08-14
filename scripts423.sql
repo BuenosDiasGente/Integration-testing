0b9206bd9e97c92b8a3d1be046752dba163a76ec
BuenosDiasGente <morkovka03121981@gmail.com>
2023-08-14 13:00:34 +0300

    SQL

--- scripts423.sql
+++ scripts423.sql
@@ -1,5 +1,5 @@
 SELECT student.name,student.age,faculty.name
-FROM student INNER JOIN faculty ON student.faculty_id=faculty.id;
+FROM student LEFT JOIN faculty ON student.faculty_id=faculty.id;
 
 SELECT student.name,student.age FROM student INNER JOIN avatar a on student.id = a.student_id;
 
